78165c8631e441d025d0bd12315c26ca7aab8225
fahmidprachaw <fahmidprachaw@outlook.com>
2026-03-31 13:04:00 +0600

    successfully register category, tag, post in admin.py

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import Category, Tag, Post
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
 
 # Register your models here.
